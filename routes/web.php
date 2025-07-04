ce9ce718edce44846a6ffdf918a8251bb092d215
iamdave <22-76345@g.batstate-u.edu.ph>
2025-07-04 21:37:54 +0800

    Created forget and reset password not finished

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
 /* ─────────── Role Test Route ─────────── */
 Route::get('/test-role', function () {
@@ -38,3 +40,9 @@ Route::middleware(['auth', 'role:journalist'])->group(function () {
 Route::middleware(['auth', 'role:user'])->group(function () {
     Route::get('/user', [PageController::class, 'user'])->name('user');
 });
+
+//reset passwortd
+Route::get('forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('reset-password', [ResetPasswordController::class, 'reset'])->name('password.update');
